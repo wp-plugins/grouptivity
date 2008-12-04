ed310c5cca78296e32bb2053a086f8c3884e4e57
MartinLogan <MartinLogan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-04 17:04:19 +0000

    2.1.0 branch
    git-svn-id: https://plugins.svn.wordpress.org/grouptivity/tags/2.1.0@77533 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shareplus.php
+++ shareplus.php
@@ -1,7 +1,7 @@
 <?php
-/*  Copyright 2007  appMail, LLC dba Grouptivity  (email : feedback@grouptivity.com)
+/*  Copyright 2008  appMail, LLC dba Grouptivity  (email : feedback@grouptivity.com)
 
-   Copyright (c) 2007 appMail, LLC dba Grouptivity
+   Copyright (c) 2008 appMail, LLC dba Grouptivity
    http://grouptivity.com/projects/wordpress
 
    This is an add-on for WordPress
@@ -14,10 +14,10 @@
    *****************************************************************
 
 Plugin Name: Share+ by Grouptivity
-Plugin URI: http://grouptivity.com/download/wordpress.html
-Description: Grouptivity's Share+ allows readers to share posts via email or on popular bookmarking services. Readers can also subscribe to popular posts from your blog on Facebook using Grouptivity's Social News Facebook Application. <a href="options-general.php?page=grouptivity/shareplus_options.php">Configuration Options are here</a>
-Version: 2.0.0
-Author: Oliver Muoto. Original modified by Martin Logan.
+Plugin URI: http://wordpress.org/extend/plugins/grouptivity/
+Description: Grouptivity's <strong>Share+</strong> allows readers to share posts via email or on popular bookmarking services. Readers can also subscribe to popular posts from your blog on Facebook using Grouptivity's Social News Facebook Application. <a href="plugins.php?page=grouptivity/shareplus_options.php">Configuration Options are here</a>. You will need to <a href="widgets.php">go to the widgets screen</a> configure the Most Shared sidebar widget
+Version: 2.1.0
+Author: Grouptivity
 Author URI: http://grouptivity.com/
 */
 
@@ -35,12 +35,10 @@ Author URI: http://grouptivity.com/
 // NO NEED TO EDIT BELOW THIS LINE
 // ============================================================
 
-//@define('AK_WPROOT', '../../../');
 @define('GTVT_FILEPATH', '/wp-content/plugins/grouptivity/shareplus.php');
 
 if (function_exists('load_plugin_textdomain')) {
 	load_plugin_textdomain('grouptivity', 'wp-content/plugins/grouptivity');
-	//load_plugin_textdomain('grouptivity.com');
 }
 
 
@@ -52,6 +50,242 @@ function gtvt_init()
 }
 add_action('init', 'gtvt_init');
 
+/* Widget */
+/* Define the sidebar widget code */
+function widget_grouptivity_init() {
+
+	// Check for the required plugin functions. This will prevent fatal
+	// errors occurring when you deactivate the dynamic-sidebar plugin.
+	if ( !function_exists('register_sidebar_widget') ) {
+		return;
+	}
+	// Load JQuery for JSONP
+	wp_enqueue_script('jquery');
+
+	// This is the function that outputs our stuff.
+	function widget_grouptivity($args) {
+		//Widget goes here
+		extract($args);
+		$options = get_option('grouptivity_widget_options');
+		$grouptivity_options = get_option('grouptivity_options');
+		$gvPartnerId 	= $grouptivity_options['partnerId'];
+		if ($options["title"]){
+			$title=$options["title"];
+		} else {
+			$title="What's Hot!";
+		}
+		$numArticles=$options["numArticles"];
+		if ($options["numDays"]){
+			$numDays=$options["numDays"];
+		} else {
+			$numDays=30;
+		}
+		if ($options["showCount"]){
+			$showCount=$options["showCount"];
+		} else {
+			$showCount="y";
+		}
+		$ulstyle=$options["ulstyle"];
+		$listyle=$options["listyle"];
+		$astyle=$options["astyle"];
+		echo $before_widget . $before_title . $title . $after_title;
+		$url = "http://www.grouptivity.com/main/api/mostShared/json.php?pId=".rawurlencode($gvPartnerId)."&lsize=$numArticles&start=0";
+		?>
+		<ul class="grouptivity-newsItemList" id="gtvtMostShared"><li class="grouptivity-newsItem">Loading...</li></ul>
+		<script type="text/javascript">
+		// Define object and functions
+		var gtvt_most_shared={
+			days: <?php echo $numDays;?>,
+			addCss:function (cssCode) {
+				var styleElement = document.createElement("style");
+				styleElement.type = "text/css";
+				if (styleElement.styleSheet) {
+					styleElement.styleSheet.cssText = cssCode;
+				} else {
+					styleElement.appendChild(document.createTextNode(cssCode));
+				}
+				document.getElementsByTagName("head")[0].appendChild(styleElement);
+			},
+			processResponse: function(data) {
+				var ul, li, a, i;
+				var ul=document.getElementById("gtvtMostShared");
+				if (data.listSize && data.listSize!=="0") {
+					ul.removeChild(ul.firstChild);
+					for  (i=0;i<data.mostShared.length;i++) {
+						li=document.createElement("li"); li.className="grouptivity-newsItem"; 
+						a=document.createElement("a"); a.className="grouptivity-newsItemA"; a.target="_blank"; a.href=data.mostShared[i].url; 
+						a.appendChild(document.createTextNode(data.mostShared[i].title));
+						li.appendChild(a);
+						ul.appendChild(li);
+						<?php if ($showCount=="y"){?>
+						var j, imgSrc, img, typeind, count;
+						// Show the couunts
+						li=document.createElement("li"); li.className="grouptivity-newsItem"; 
+						for (j=0;j<data.mostShared[i].shareStats.length;j++) {
+							typeind=data.mostShared[i].shareStats[j].typeind;
+							count=data.mostShared[i].shareStats[j].count;
+							switch (typeind) {
+							case "sharing":
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/email.gif";
+								break;
+							case "GoogleBookmarks":
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/google.gif";
+								break;
+							case "WindowsLive":
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/windows.gif";
+								break;
+							case "YahooWeb":
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/yahoo.gif";
+								break;
+							case "YahooBookmarks":
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/yahoobm.gif";
+								break;
+							case "Technocrati":
+								//Oops! embasassing spelling faux pas
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/technorati.gif";
+								// $bkmark->typeind="Technorati";
+								break;
+							default:
+								imgSrc="http://cdn.grouptivity.com/main/api/webjs/images/"+typeind.toLowerCase()+".gif";
+							}
+							img=document.createElement("img"); img.align="bottom"; img.border="0"; img.title=typeind; img.alt=typeind; img.src=imgSrc;
+							if (typeind==="sharing") {
+								li.insertBefore(document.createTextNode(" "+count+" "), li.firstChild);
+								li.insertBefore(img, li.firstChild);
+
+							} else if (typeind!=="reading") {
+								li.appendChild(img); 
+								li.appendChild(document.createTextNode(" "+count+" "));
+							}
+							ul.appendChild(li);
+						}
+						<?php } ?>
+					}
+					//Append More link
+					li=document.createElement("li");
+					a=document.createElement("a"); a.className="grouptivity-newsItemA"; a.target="_blank"; a.href="<?php print "http://apps.grouptivity.com/socialmail/mostShared.do?pId=".rawurlencode($gvPartnerId); ?>"; 
+					a.appendChild(document.createTextNode("More..."));
+					li.appendChild(a);
+					ul.appendChild(li);
+
+				} else {
+					if (gtvt_most_shared.days===1) {
+						gtvt_most_shared.days=7;
+						jQuery.getJSON("<?php echo $url; ?>&day="+encodeURIComponent(gtvt_most_shared.days)+"&jsonp=?", gtvt_most_shared.processResponse);	
+					} else if (gtvt_most_shared.days===7) {
+						gtvt_most_shared.days=30;
+						jQuery.getJSON("<?php echo $url; ?>&day="+encodeURIComponent(gtvt_most_shared.days)+"&jsonp=?", gtvt_most_shared.processResponse);	
+
+					} else {
+						ul.removeChild(ul.firstChild);
+						li=document.createElement("li"); li.className="grouptivity-newsItem"; 
+						li.appendChild(document.createTextNode("Nothing to see."));
+						ul.appendChild(li);
+					}
+				}
+
+			}
+		};
+		//Call for data
+		jQuery.getJSON("<?php echo $url; ?>&day="+encodeURIComponent(gtvt_most_shared.days)+"&jsonp=?",
+        	gtvt_most_shared.processResponse
+       		 );
+		// Add CSS
+		gtvt_most_shared.addCss(<?php print "\".grouptivity-newsItemList{$ulstyle} .grouptivity-newsItem{$listyle} .grouptivity-newsItemA, .grouptivity-newsItemA:visited {$astyle}\""; ?>);
+		</script>
+		<?php
+		echo $after_widget;
+	}
+	// control panel
+	function widget_grouptivity_control() {
+		$options = $newoptions = get_option('grouptivity_widget_options');
+		$grouptivity_options = get_option('grouptivity_options');
+		$gvPartnerId 	= $grouptivity_options['partnerId'];
+		// defaults
+		if(!$options) {
+			$options['title'] = $newoptions['title']  = 'What\'s Hot!';
+			$options['numArticles'] = $newoptions['numArticles']  = 10;
+            		$options['numDays'] = $newoptions['numDays'] = 30;
+            		$options['ulstyle'] = $newoptions['ulstyle'] = '';
+            		$options['listyle'] = $newoptions['listyle'] = '';
+            		$options['astyle'] = $newoptions['astyle'] = '';
+            		$options['showCount'] = $newoptions['showCount'] = 'y';
+		}
+		if ( $_POST["grouptivity-submit"] ) {
+			$newoptions['title'] = trim(strip_tags(stripslashes($_POST["grouptivity-title"])));
+			$newoptions['numArticles'] = trim(strip_tags(stripslashes($_POST["grouptivity-numArticles"])));
+			$newoptions['numDays'] = trim(strip_tags(stripslashes($_POST["grouptivity-numDays"])));
+			$newoptions['ulstyle'] = trim(strip_tags(stripslashes($_POST["grouptivity-ulstyle"])));
+			$newoptions['listyle'] = trim(strip_tags(stripslashes($_POST["grouptivity-listyle"])));
+			$newoptions['astyle'] = trim(strip_tags(stripslashes($_POST["grouptivity-astyle"])));
+			$newoptions['showCount'] = trim(strip_tags(stripslashes($_POST["grouptivity-showCount"])));
+		}
+
+		if ( $options != $newoptions ) {
+			$options = $newoptions;
+			update_option('grouptivity_widget_options', $options);
+		}
+		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		$numArticles = htmlspecialchars($options['numArticles'], ENT_QUOTES);
+		$numDays = htmlspecialchars($options['numDays'], ENT_QUOTES);
+		$ulstyle = htmlspecialchars($options['ulstyle'], ENT_QUOTES);
+		$listyle = htmlspecialchars($options['listyle'], ENT_QUOTES);
+		$astyle = htmlspecialchars($options['astyle'], ENT_QUOTES);
+		$showCount = htmlspecialchars($options['showCount'], ENT_QUOTES);
+
+			?>
+	<!-- HTML form for widget options -->	
+	<p><label for="grouptivity-title">Title:</label><input style="width: 100%;" id="grouptivity-title" name="grouptivity-title" type="text" value="<?php echo $title; ?>" /></p>
+	<?php if (trim($gvPartnerId)!="") {?>
+	<p>Publisher ID: <?php echo $gvPartnerId; ?></p>
+	<?php } else { ?>
+	<p>You need a grouptivity publisher ID for this widget to function. <a href="plugins.php?page=grouptivity/shareplus_options.php">Please click here to enter your publisher ID</a></p>
+	<?php } ?>
+
+	<p><label for="grouptivity-numArticles">No. of items to display:</label><select id="grouptivity-numArticles" name="grouptivity-numArticles"/>
+		<option value="1" <?php echo ($numArticles == '1' ? 'selected' : '') ?>>1</option>
+		<option value="2" <?php echo ($numArticles == '2' ? 'selected' : '') ?>>2</option>
+		<option value="3" <?php echo ($numArticles == '3' ? 'selected' : '') ?>>3</option>
+		<option value="4" <?php echo ($numArticles == '4' ? 'selected' : '') ?>>4</option>
+		<option value="5" <?php echo ($numArticles == '5' ? 'selected' : '') ?>>5</option>
+		<option value="6" <?php echo ($numArticles == '6' ? 'selected' : '') ?>>6</option>
+		<option value="7" <?php echo ($numArticles == '7' ? 'selected' : '') ?>>7</option>
+		<option value="8" <?php echo ($numArticles == '8' ? 'selected' : '') ?>>8</option>
+		<option value="9" <?php echo ($numArticles == '9' ? 'selected' : '') ?>>9</option>
+		<option value="10" <?php echo ($numArticles == '10' ? 'selected' : '') ?>>10</option>
+	</select>
+	</p>
+	<p><label for="grouptivity-numDays">No. of days data to use:</label><select id="grouptivity-numDays" name="grouptivity-numDays"/>
+		<option value="1" <?php echo ($numDays == '1' ? 'selected' : '') ?>>1</option>
+		<option value="7" <?php echo ($numDays == '7' ? 'selected' : '') ?>>7</option>
+		<option value="30" <?php echo ($numDays == '30' ? 'selected' : '') ?>>30</option>
+	</select>
+	</p>
+	<p><label for="grouptivity-showCount">Show shared counts:</label><select id="grouptivity-showCount" name="grouptivity-showCount"/>
+		<option value="y" <?php echo ($showCount == 'y' ? 'selected' : '') ?>>Yes</option>
+		<option value="n" <?php echo ($showCount == 'n' ? 'selected' : '') ?>>No</option>
+	</select>
+	</p>
+	<p>Advanced configuration</p>
+	<p><label for="grouptivity-ulstyle">UL Style:</label><input style="width: 100%;" id="grouptivity-ulstyle" name="grouptivity-ulstyle" type="text" value="<?php echo $ulstyle; ?>" /></p>
+	<p><label for="grouptivity-listyle">LI Style:</label><input style="width: 100%;" id="grouptivity-listyle" name="grouptivity-listyle" type="text" value="<?php echo $listyle; ?>" /></p>
+	<p><label for="grouptivity-astyle">A Style:</label><input style="width: 100%;" id="grouptivity-astyle" name="grouptivity-astyle" type="text" value="<?php echo $astyle; ?>" /></p>
+    
+	<input type="hidden" id="grouptivity-submit" name="grouptivity-submit" value="1" />
+	<?php
+	}
+
+	// This registers our widget so it appears with the other available
+	// widgets and can be dragged and dropped into any active sidebars.
+	$widget_ops = array('classname' => 'widget_grouptivity_mostshared', 'description' => __( "Your blogs most shared posts") );
+	// THe below did not let me create a description - SO used wp_reg...
+	// register_sidebar_widget( __('Most Shared'), 'widget_grouptivity');
+	wp_register_sidebar_widget('grouptivity_mostshared', __('Most Shared'), 'widget_grouptivity', $widget_ops);
+	register_widget_control('grouptivity_mostshared', 'widget_grouptivity_control');
+
+}
+add_action('plugins_loaded', 'widget_grouptivity_init');
+
 
 /* HEADER */
 
@@ -61,10 +295,22 @@ function gtvt_header()
 
 	$grouptivity_options = get_option('grouptivity_options');
 
+	if (!$grouptivity_options) {
+		// Set defaults
+		$grouptivity_options['hideFBApp'] 	= 0;
+		$grouptivity_options['hideClip'] 	= 0;
+		$grouptivity_options['hideSocial'] 	= 0;
+		$grouptivity_options['emailNote'] 	= 'Check this out...';
+		$grouptivity_options['subjectPrefix'] 	= 'Let\'s discuss ';
+		$update_grouptivity_queries[] = update_option('grouptivity_options', $grouptivity_options);
+	}
+
 	$gvPartnerId 	= $grouptivity_options['partnerId'];
 	$gvHideFBApp 	= $grouptivity_options['hideFBApp'];
 	$gvHideClip 	= $grouptivity_options['hideClip'];
 	$gvHideSocial 	= $grouptivity_options['hideSocial'];
+	$gvEmailNote 	= $grouptivity_options['emailNote'];
+	$gvSubjectPrefix= $grouptivity_options['subjectPrefix'];
 
 	if (empty($gvHideFBApp))
 		$gvHideFBApp=0;
@@ -77,21 +323,26 @@ function gtvt_header()
 	if (GTVT_SHOWCATEGORIES)
 		$gvShowCats= 1;
 
-	/* Output JS and CSS needed by the popup in the header */
-
-	print('
-<!-- Start Of Script Generated By Share+ Grouptivity 2.0 -->
-<script type="text/javascript">
-	var gtvt_partner_id="'.$gvPartnerId.'";
-	var gtvt_allow_cats='.$gvShowCats.';
-	var gtvt_hide_fbapp='.$gvHideFBApp.';
-	var gtvt_hide_clip='.$gvHideClip.';
-	var gtvt_hide_social='.$gvHideSocial.';');
-
-	/* Color Customization Settings */
+	/* Customization Settings */
 	$gvBorderColor 	= $grouptivity_options['borderColor'];
 	$gvShadowColor 	= $grouptivity_options['shadowColor'];
 	$gvBannerColor 	= $grouptivity_options['bannerColor'];
+
+	/* Output JS and CSS needed by the popup in the header */
+
+	print("
+<!-- Start Of Script Generated By Share+ Grouptivity 2.0 -->
+<script type=\"text/javascript\">
+	var gtvt_partner_id=\"$gvPartnerId\";
+	var gtvt_allow_cats=$gvShowCats;
+	var gtvt_hide_fbapp=$gvHideFBApp;
+	var gtvt_hide_clip=$gvHideClip;
+	var gtvt_hide_social=$gvHideSocial;
+	var gtvt_email_note=\"$gvEmailNote\";
+	var gtvt_subject_prefix=\"$gvSubjectPrefix\";");
+	//var gtvt_services='grouptivity|facebook|myspace|digg|delicious|google|windows|yahoo|stumbleupon|reddit|technorati|ask|furl|blinklist|friendfeed|twitter|fark|magnolia|misterwong|newsvine|slashdot|tailrank|yahoobm';
+	
+
 
 	$nl = "\n";
 	$tb = "\t";
@@ -157,10 +408,7 @@ function gtvt_add_link($content)
 	/* Get title of the post */
 	$gtvtTitle = get_the_title();
 	$gtvtTitle = preg_replace("/'/","&#39;",$gtvtTitle);
-	//$gtvtTitle = escapeString ($gtvtTitle);
 	$gtvtTitle = rawurlencode($gtvtTitle);
-	//$gtvtTitle = fixQuotes ($gtvtTitle);
-	//$gtvtTitle = htmlspecialchars ($gtvtTitle);
 
 	$gtvtInfo = "Share this post...";
 
@@ -176,7 +424,6 @@ function gtvt_add_link($content)
 	}
 	$gtvtExcerpt = preg_replace("/'/","&#39;",$gtvtExcerpt);
 	$gtvtExcerpt = rawurlencode($gtvtExcerpt);
-	//$gtvtExcerpt = escapeString ($gtvtExcerpt);
 	$gtvtSummary =  $gtvtExcerpt;
 
 	/* Get URL to Share+ button */
@@ -206,7 +453,6 @@ function gtvt_add_link($content)
 	return $content;
 }
 add_action('the_content', 'gtvt_add_link');
-//add_action('the_content_rss', 'gtvt_add_link');
 
 /* FOOTER */
 
@@ -239,12 +485,25 @@ add_action('wp_footer', 'gtvt_footer');
 
 function grouptivity_config_menu()
 {
-	if (function_exists('add_options_page')) {
+	if ( function_exists('add_submenu_page') ) {
+		add_submenu_page('plugins.php', __('Share+ by Grouptivity'), __('Share+'), 'manage_options', 'grouptivity/shareplus_options.php') ;
+	} else if (function_exists('add_options_page')) {
 		add_options_page(__('Share+ by Grouptivity'), __('Share+'), 'manage_options', 'grouptivity/shareplus_options.php') ;
 	}
 }
 add_action('admin_menu', 'grouptivity_config_menu');
-
+/* Warn if no pId */
+/* Programming note - need to extend condition below */
+$grouptivity_options = get_option('grouptivity_options');
+if ( !$grouptivity_options || $grouptivity_options['partnerId']==="") {
+	function gtvtpid_warning() {
+		echo "
+		<div id='gtvtpid-warning' class='updated fade'><p><strong>".__('Share+ is ready.')."</strong> ".sprintf(__('But you must <a href="%1$s">enter your Grouptivity publisher ID</a> to get the most out of it.'), "plugins.php?page=grouptivity/shareplus_options.php")."</p></div>
+		";
+	}
+	add_action('admin_notices', 'gtvtpid_warning');
+	return;
+}
 function escapeString($str)
 {
 	$str = str_replace(array('\\', "'"), array("\\\\", "\\'"), $str);
@@ -253,20 +512,5 @@ function escapeString($str)
 	return $str;
 }
 
-function fixQuotes ($str)
-{
-    $badchr        = array(
-        "\xc2", // prefix 1
-        "\x80", // prefix 2
-        "\x98", // single quote opening
-        "\x99", // single quote closing
-        "\x8c", // double quote opening
-        "\x9d"  // double quote closing
-    );
-
-    $goodchr    = array('', '', '\'', '\'', '"', '"');
-
-    return (str_replace($badchr, $goodchr, $str));
-}
 
 ?>
--- shareplus_options.php
+++ shareplus_options.php
@@ -9,6 +9,8 @@ if($_POST['Submit']) {
 	$grouptivity_options['hideClip'] 	= intval(trim($_POST['hideClip']));
 	$grouptivity_options['hideSocial'] 	= intval(trim($_POST['hideSocial']));
 
+	$grouptivity_options['emailNote'] 	= addslashes($_POST['emailNote']);
+	// $grouptivity_options['subjectPrefix'] 	= addslashes($_POST['subjectPrefix']);
 	$grouptivity_options['borderColor'] 	= addslashes($_POST['borderColor']);
 	$grouptivity_options['shadowColor'] 	= addslashes($_POST['shadowColor']);
 	$grouptivity_options['bannerColor'] 	= addslashes($_POST['bannerColor']);
@@ -37,7 +39,7 @@ $grouptivity_options = get_option('grouptivity_options');
 $plugin_uri = get_option('siteurl')."/wp-content/plugins/grouptivity/";
 
 ?>
-<script type="text/javascript" src="http://apps.grouptivity.com/partner/jsp/getPublisherIdJS.jsp?pId=<?php echo $grouptivity_options['partnerId'] ?>"></script>
+
 <script language="javascript">
 	function helpOn(id){
 		document.getElementById(id).style.display='block';
@@ -53,14 +55,18 @@ $plugin_uri = get_option('siteurl')."/wp-content/plugins/grouptivity/";
 <?php if(!empty($text)) { echo '<!-- Last Action --><div id="message" class="updated fade"><p>'.$text.'</p></div>'; } ?>
 <div class="wrap">
 <h2><?php _e('Share+ Configuration'); ?></h2>
-<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" style="text-align:left">
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" style="text-align:left" id="gtvtSettings" name="gtvtSettings">
 			<fieldset class="options">
+				<legend><?php _e('Most Shared Widget'); ?></legend>
+				<p><a href="widgets.php">Click here to configure the Most shared sidebar widget</a></p>
 					<legend><?php _e('Account:'); ?></legend>
 						<table width="100%"  border="0" cellspacing="3" cellpadding="3">
 							<tr valign="top">
 								<th align="left" width="30%"><?php _e('Publisher ID'); ?></th>
-								<td align="left"><input type="text" name="partnerId" value="<?php echo $grouptivity_options['partnerId'] ?>" /> <span id="validate"></span><br/>
-								<span style="color:#999999;font-size:11px">If you don't have a partner ID and wish to track sharing activity on your site. Sign up as a <a style="color:#999999" target="_blank" href="http://www.grouptivity.com/main/signup.php">Grouptivity Publisher</a></span>
+								<td align="left"><input type="text" name="partnerId" id="partnerId" value="<?php echo $grouptivity_options['partnerId'] ?>" /> <span id="validate"></span><br/>
+								<span style="font-size:12px;">
+								<span id="gtvt-acct-logon" style="font-size:12px;display:none"><a target="_blank" href="http://www.grouptivity.com/main/login.php">Click here to login to your Grouptivity Account</a> to access reporting and configuration.</span>
+								<span id="gtvt-message" style="font-size:12px;">To be able to serve advertizing and have access to reporting please go to <a style="color:#000000" target="_blank" href="http://www.grouptivity.com/main/signup.php">Grouptivity Publisher Sign up</a> <br/>If you have already signed up the Publisher ID will be in the confirmation email you received.</span>
 
 								</td>
 							</tr>
@@ -105,6 +111,18 @@ $plugin_uri = get_option('siteurl')."/wp-content/plugins/grouptivity/";
 					<legend><?php _e('Customize:'); ?></legend>
 						<table width="100%"  border="0" cellspacing="3" cellpadding="3">
 							<tr valign="top">
+								<th align="left" width="30%"><?php _e('Email Note Text'); ?></th>
+								<td align="left">
+									<input type="text" name="emailNote" width="8" value="<?php echo $grouptivity_options['emailNote'] ?>" /> <span style="font-size:11px;color:#999999">Example: "Check this out....."</span>
+								</td>
+							</tr>
+							<!-- <tr valign="top">
+								<th align="left" width="30%"><?php _e('Email Subject Prefix'); ?></th>
+								<td align="left">
+									<input type="text" name="subjectPrefix" width="8" value="<?php echo $grouptivity_options['subjectPrefix'] ?>" /> <span style="font-size:11px;color:#999999">Example: "Let&#39;s discuss.."</span>
+								</td>
+							</tr> -->
+							<tr valign="top">
 								<th align="left" width="30%"><?php _e('Border Color'); ?></th>
 								<td align="left">
 									<input type="text" name="borderColor" width="8" value="<?php echo $grouptivity_options['borderColor'] ?>" /> <span style="font-size:11px;color:#999999">Example: #FF0000</span>
@@ -130,16 +148,37 @@ $plugin_uri = get_option('siteurl')."/wp-content/plugins/grouptivity/";
 
 </form>
 </div>
-<script>
-	var validateObj = document.getElementById("validate");
-	if (typeof(pId)=='undefined')
-		pId="";
-
-	if (pId=="invalid")
-	{
-		validateObj.innerHTML='<img align="absmiddle" src="<?php echo $plugin_uri ?>/images/error.gif" width="16" height="16" border="0" /><b style="color:#ff0000"> Missing or Invalid</b>';
-	} else if ((pId!="invalid") && (pId!=""))
-	{
-		validateObj.innerHTML='<img align="absmiddle" src="<?php echo $plugin_uri  ?>/images/valid.gif" width="16" height="16" border="0" /><span style="color:#008080"> Valid</span>';
+<script type="text/javascript">
+	var savepid=false;
+	function gtvt_validatepid() {
+		var validateObj = document.getElementById("validate");
+		if (typeof(pId)=='undefined') {pId="";}
+	
+		if (pId=="invalid") {
+			validateObj.innerHTML='<img align="absmiddle" src="<?php echo $plugin_uri ?>/images/error.gif" width="16" height="16" border="0" /><b style="color:#ff0000"> Missing or Invalid</b>';
+		} else if ((pId!="invalid") && (pId!="")) {
+			validateObj.innerHTML='<img align="absmiddle" src="<?php echo $plugin_uri  ?>/images/valid.gif" width="16" height="16" border="0" /><span style="color:#008080"> Valid</span>';
+			document.getElementById("gtvt-message").style.display="none";
+			document.getElementById("gtvt-acct-logon").style.display="block";
+			if (savepid) {document.gtvtSettings.submit();};
+		}
 	}
+<?php
+if (!$grouptivity_options || $grouptivity_options['partnerId']=="") {
+?>
+
+	function gtvt_pidcallback(data) {
+		if (data && data.pId) {
+			document.gtvtSettings.partnerId.value=data.pId;
+			jQuery.getScript("http://apps.grouptivity.com/partner/jsp/getPublisherIdJS.jsp?pId="+encodeURIComponent(data.pId), gtvt_validatepid);
+		}
+	}
+	jQuery.ready(jQuery.getJSON("http://grouptivity.com/main/api/mostShared/pubid.php?jsonp=?", gtvt_pidcallback));
+<?php
+} else {
+?>
+	jQuery.getScript("http://apps.grouptivity.com/partner/jsp/getPublisherIdJS.jsp?pId="+encodeURIComponent("<?php print $grouptivity_options['partnerId']; ?>"), gtvt_validatepid);
+<?php
+	}
+?>
 </script>
